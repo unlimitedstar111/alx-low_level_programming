a0209304dc3fecbe7b1cc66740a4317680bbc393
star <star@gmail.com>
2023-06-19 15:51:30 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+int num;
+
+while (num < 10);
+
+{
+	putchar("%d", num);
+	num += 1;
+}
+putchar("\n");
+return (0);
+}
