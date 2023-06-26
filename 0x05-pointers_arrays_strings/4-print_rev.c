bf44fc7e2f4392168950921e1871cb95b73c83cb
star <star@gmail.com>
2023-06-26 14:27:46 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,27 @@
-#include "main,h"
+#include "main.h"
 
 /**
- *
+ * print_rev -  a function that prints a string
+ * @s: string
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+		int o;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	s--;
+	for (o = length; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
