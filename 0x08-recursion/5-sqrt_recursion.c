cb43f2645eca219a79527518f075b27d75ab786b
star <star@gmail.com>
2023-07-06 06:09:11 -0700

    sqrt recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include"main.h"
+
+/**
+ * _sqrt_recursion - function that returns natural square root
+ * @n: number to calculate square root from
+ * Return: result of square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find the natural square root
+ * of a number
+ * @n: number to calculate square root from
+ * @i: iterator
+ * Return: result of square root
+ */
+
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else if (i * i == n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (actual_sqrt_recursion(n, i + 1));
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -17,3 +17,5 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 
 int wildcmp(char *s1, char *s2);
+
+int actual_sqrt_recursion(int n, int i);
